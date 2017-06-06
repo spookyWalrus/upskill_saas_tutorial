b799f0e4bc1e6146a429fb376ec44ee9588b7794
Zen Dude <zenzenzen@gmail.com>
2017-06-06 16:24:42 +0000

    Added Mailer settings to environment file

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ActionMailer::Base
-    default to: 'companyemail@example.com' 
+    default to: 'zenzenzen@gmail.com' 
     
     def contact_email(name, email, body)
       @name = name
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,14 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+    :port       => ENV['MAILGUN_SMTP_PORT'],
+    :address    => ENV['MAILGUN_SMTP_PORT'],
+    :user_name  => ENV['MAILGUN_SMTP_PORT'],
+    :password   => ENV['MAILGUN_SMTP_PORT'],
+    :domain     => 'obscure-castle-25165.herokuapp.com'
+    :authentication => :plain,
+}
+
+ActionMailer::Base.delivery_method = :smtp
