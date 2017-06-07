8d250cd200b786b5d896d61d7c17e1378658641b
Zen Dude <zenzenzen@gmail.com>
2017-06-07 14:05:36 +0000

    Changed default email for Heroku

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'zenzenzen@gmail.com'
+  default from: 'bananamonkeyape@gmail.com'
   layout 'mailer'
 end
--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ActionMailer::Base
-    default to: 'zenzenzen@gmail.com' 
+    default to: 'bananamonkeyapegmail.com' 
     
     def contact_email(name, email, body)
       @name = name
