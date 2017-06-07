fcc52a5660663fc61830ffae7b63322af2ccdd20
Zen Dude <zenzenzen@gmail.com>
2017-06-07 14:57:24 +0000

    Updated contacts_controller file, troubleshooting email issues

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -10,7 +10,6 @@ class ContactsController < ApplicationController
         email = params[:contact][:email]
         body = params[:contact][:comments]
         ContactMailer.contact_email(name,email,body).deliver
-        
         flash[ :success] = "Message sent."
         redirect_to new_contact_path
       else 
