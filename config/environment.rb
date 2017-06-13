392fda133f7cd0cf5794be42136d06888b2e9dd4
Zen Dude <zenzenzen@gmail.com>
2017-06-13 16:50:31 +0000

    Updated mailer settins to environment file

--- config/environment.rb
+++ config/environment.rb
@@ -6,9 +6,9 @@ Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
     :port       => ENV['MAILGUN_SMTP_PORT'],
-    :address    => ENV['MAILGUN_SMTP_PORT'],
-    :user_name  => ENV['MAILGUN_SMTP_PORT'],
-    :password   => ENV['MAILGUN_SMTP_PORT'],
+    :address    => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name  => ENV['MAILGUN_SMTP_LOGIN'],
+    :password   => ENV['MAILGUN_SMTP_PASSWORD'],
     :domain     => 'obscure-castle-25165.herokuapp.com',
     :authentication => :plain,
 }
