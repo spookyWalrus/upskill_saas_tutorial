6fbe2c356f1f0118ceb6176db4268e43285ff4ca
Zen Dude <zenzenzen@gmail.com>
2017-06-27 15:09:44 +0000

    Updated contacts_controller, contact_mailer and routes.rb file. Some buggy issues when operating rails console

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ActionMailer::Base
-    default to: 'companyemail@example.com' 
+    default to: 'bananamonkeyape@gmail.com' 
     
     def contact_email(name, email, body)
       @name = name
